966b34ac0d99845ae0b44be971a3c023b218c7ab
Oppah21 <matemaoppah@gmail.com>
2025-09-19 10:02:49 +0200

    serving JSON data

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-// SERVING HTML PAGES
+// SERVING JSON DATA
  var http = require('http');
  var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-myReadStream.pipe(res);
-    });
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var myObj = {
+        name: 'Oppah',
+        job: 'Ninja',
+        age: 24
+    };
+
+    res.end(JSON.stringify(myObj));
+});
 server.listen(3000, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
+// SERVING HTML PAGES
+//  var http = require('http');
+//  var fs = require('fs');
 
-
-
-
-
-
-
+// var server = http.createServer(function(req, res) {
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
+// myReadStream.pipe(res);
+//     });
+// server.listen(3000, '127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
 
 // PIPES
 //  var http = require('http');
