85825f8d828d09785832c3f8c254e5bc43106b0b
Oppah21 <matemaoppah@gmail.com>
2025-09-19 09:44:22 +0200

    html pages

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-// PIPES
+// SERVING HTML PAGES
  var http = require('http');
  var fs = require('fs');
 
-
 var server = http.createServer(function(req, res) {
     console.log('request was made: ' + req.url);
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
 myReadStream.pipe(res);
-    // res.end('Hello Oppah');
-     });
+    });
 server.listen(3000, '127.0.0.1');
 console.log('yo dawgs, now listening to port 3000');
 
+
+
+
+
+
+
+
+// PIPES
+//  var http = require('http');
+//  var fs = require('fs');
+
+// var server = http.createServer(function(req, res) {
+//     console.log('request was made: ' + req.url);
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+// myReadStream.pipe(res);
+//     // res.end('Hello Oppah');
+//      });
+// server.listen(3000, '127.0.0.1');
+// console.log('yo dawgs, now listening to port 3000');
 
 // WRITABLE STREAMS
 //  var http = require('http');
