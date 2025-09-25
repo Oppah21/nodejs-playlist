f661cc1cd522dcb323db0054277b4050c150ff16
Oppah21 <matemaoppah@gmail.com>
2025-09-25 10:17:25 +0200

    template engines

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-// ROUTE PARAMETERS #25
+// ROUTE PARAMETERS #24
+// TEMPLATE ENGINES #25
 var express = require('express');
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-    res.send('This is the homepage');
+    res.sendFile(__dirname + '/index.html');
 
 });
 app.get('/contact', function(req, res){
-    res.send('This is the contact page');
+    res.sendFile(__dirname + '/contact.html');
 
 });
 app.get('/profile/:name', function(req, res){
-    res.send('You requested to see a profile with name ' + req.params.name);
+
+
+    var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
+    res.render('profile', {person: req.params.name , data: data});
 });
 
 app.listen(3000);
